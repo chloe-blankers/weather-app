05484a82a20d652311d8a2fa54ffbabe05c119a4
Chloe Blankers <chloeblankers@hotmail.com>
2023-07-20 07:57:58 -0700

    fixes

--- backend.js
+++ backend.js
@@ -11,26 +11,30 @@ app.use(cors());
 app.get("/weather", (req, res) => {
   let { city, country } = req.query;
   const apiKey = process.env.REACT_APP_API_KEY;
+  const apiURL = process.env.REACT_APP_API_URL;
 
   // without space it returns hong kong. Didn't see this with any other location. Did not have time to find root cause.
-  if (city.toLowerCase() === "victoria") {
+  if (city === "victoria" || city === "Victoria") {
     city = " victoria";
   }
-
-  axios
-    .get(`http://api.openweathermap.org/data/2.5/weather`, {
-      params: {
-        q: `${city},${country}`,
-        units: "metric",
-        appid: apiKey,
-      },
-    })
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Error fetching weather data" });
-    });
+  if (city === undefined && country == undefined) {
+    res.json("Waiting for user to make api call");
+  } else {
+    axios
+      .get(`${apiURL}/weather`, {
+        params: {
+          q: `${city},${country}`,
+          units: "metric",
+          appid: apiKey,
+        },
+      })
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
 });
 
 app.listen(PORT, () => {
